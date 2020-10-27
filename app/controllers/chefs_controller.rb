3cd5b5d2014ecbaf2c92b3a3555d2547e6f2eb04
Tara Matthews <matthews.s.tara@gmail.com>
2020-10-27 11:47:10 +0100

    chef show, edit

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,4 +4,38 @@ class ChefsController < ApplicationController
     @chef = Chef.new
   end
 
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @chef = Chef.find(params[:id])
+  end
+
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+  
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to @chef
+    else
+      render 'edit'
+    end  
+  end
+
+  private
+  
+  def chef_params
+    params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+  end
+
 end
