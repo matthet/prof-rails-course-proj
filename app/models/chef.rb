516a941214e1a0667103cf275600dc7c397c0c4f
Tara Matthews <matthews.s.tara@gmail.com>
2020-10-27 13:17:17 +0100

    chef listing

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,5 +5,5 @@ class Chef < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_many :recipes
   has_secure_password
-  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true 
 end
