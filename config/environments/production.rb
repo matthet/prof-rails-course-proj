0a896eb662671c36e272edd393d4473aff21d8ff
Tara Matthews <matthews.s.tara@gmail.com>
2020-10-27 19:32:24 +0100

    make app production ready with actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.web_socket_server_url = "wss://matthet-myrecipes.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://matthet-myrecipes.herokuapp.com', 'http://matthet-myrecipes.herokuapp.com']
 end
